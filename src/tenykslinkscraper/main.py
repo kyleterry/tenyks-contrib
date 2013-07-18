2a5e48645be6d05a4aba63d710a86e0e195ac6b1
Quinlan Pfiffer <shithouse@goatse.cx>
2013-07-17 21:30:22 -0700

    Change __init__ to use re.search instead of .match because .match don't work

--- src/tenykslinkscraper/main.py
+++ src/tenykslinkscraper/main.py
@@ -1,4 +1,6 @@
 import requests
+import re
+import logging
 from tenyksclient.client import Client, run_client, settings
 
 
@@ -8,6 +10,24 @@ class TenyksLinkScraper(Client):
     irc_message_filters = {
         'link_posted': [r'\(?\b(http|https)://[-A-Za-z0-9+&@#/%?=~_()|!:,.;]*[-A-Za-z0-9+&@#/%=~_()|]'],
     }
+
+    # MONKEYPATCHING IS DUMB
+    def __init__(self, name):
+            self.channels = [settings.BROADCAST_TO_CLIENTS_CHANNEL]
+            self.name = name.lower().replace(' ', '')
+            if self.irc_message_filters:
+                self.re_irc_message_filters = {}
+                for name, regexes in self.irc_message_filters.iteritems():
+                    if not name in self.re_irc_message_filters:
+                        self.re_irc_message_filters[name] = []
+                    if isinstance(regexes, basestring):
+                        regexes = [regexes]
+                    for regex in regexes:
+                        self.re_irc_message_filters[name].append(
+                            re.compile(regex).search)
+            if hasattr(self, 'recurring'):
+                gevent.spawn(self.run_recurring)
+            self.logger = logging.getLogger(self.name)
 
     def handle_link_posted(self, data, match):
         if settings.POST_URL is None:
