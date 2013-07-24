44e51a30626dd0c483744ad3ab4274826f547360
Kyle Terry <kyle@kyleterry.com>
2013-07-24 11:14:54 -0700

    catnip

---
+++ src/tenyksscripts/scripts/catnip.py
@@ -0,0 +1,3 @@
+def run(data, settings):
+    if data['payload'] == 'what happens when cats find catnip?':
+        return 'http://lovemeow.com/2013/06/when-wesley-the-cat-finds-catnip-while-his-humans-are-out/'
