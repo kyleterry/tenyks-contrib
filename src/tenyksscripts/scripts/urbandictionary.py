a43994fe9a59426bcf5fb941556e57e461ca5d4f
Nick Tzaperas <nick@nijotz.com>
2013-08-16 16:04:39 -0700

    Parse HTML to get word and first definition

--- src/tenyksscripts/scripts/urbandictionary.py
+++ src/tenyksscripts/scripts/urbandictionary.py
@@ -1,6 +1,23 @@
 import requests
+from HTMLParser import HTMLParser
+from BeautifulSoup import BeautifulSoup
 
 def run(data, settings):
     if data['payload'] == 'urban dictionary me':
         r = requests.get('http://www.urbandictionary.com/random.php')
-        return r.url
+
+        soup = BeautifulSoup(r.text,
+            convertEntities=BeautifulSoup.HTML_ENTITIES)
+
+        # The word is inner text of the child span of the td with class 'word'
+        word = soup.findAll('td', attrs={'class': 'word'})[0].findChild().text
+
+        # Definitions are just innertext of divs with class definition
+        definition_divs = soup.findAll('div', attrs={'class': 'definition'})
+
+        # BS doesn't unescape hex html encoded characaters, need HTMLParser
+        parser = HTMLParser()
+        definitions = [ parser.unescape(div.text) for div in definition_divs ]
+
+        # Just use the first definition
+        return '{0} - {1}'.format(word, definitions[0])
