7384955e62a9c491e53c820aee066221fb887e62
Kyle Terry <kyle@kyleterry.com>
2013-10-29 14:48:47 -0700

    blah

--- src/tenyksscripts/scripts/githubstatus.py
+++ src/tenyksscripts/scripts/githubstatus.py
@@ -6,7 +6,7 @@ def run(data, settings):
         status_url = 'https://status.github.com/api/status.json'
         last_message_url = 'https://status.github.com/api/last-message.json'
         status = requests.get('https://status.github.com/api/status.json').json()
-        if status in ('minor', 'major'):
+        if status['status'] in ('minor', 'major'):
             last_message = requests.get(last_message_url)
             return 'Github is having problems as of {timestamp}: {message}'.format(
                     timestamp=last_message['created_on'], message=last_message['body'])
