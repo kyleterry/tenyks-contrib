fc79296228c7231e1eaefb2baac143f806f9d727
Kyle Terry <kyle@kyleterry.com>
2013-11-13 21:27:18 -0800

    Added gentoo client

---
+++ src/gentoo/main.py
@@ -0,0 +1,43 @@
+from os.path import abspath, join, dirname
+import sys
+import logging
+import re
+import time
+
+from tenyks.client import Client, run_client
+from tenyks.client.config import settings
+
+logger = logging.getLogger('tenyks-contrib.gentooservice')
+
+
+class GentooService(Client):
+
+    irc_message_filters = {
+        'find_gentoo': [re.compile(r'\b(gentoo)\b', flags=re.IGNORECASE).search],
+        'get_last_mention': [r'^last gentoo mention$']
+    }
+
+    direct_only = False
+
+    def __init__(self, *args, **kwargs):
+        super(TenyksScripts, self).__init__(*args, **kwargs)
+        self.filename = 'gentoo_last_mention'
+
+    def handle_find_gentoo(self, data, match):
+        last_mention = time.time()
+        with open(join(settings.DATA_WORKING_DIR, self.filename), 'w') as f:
+            f.write(str(last_mention))
+
+    def handle_get_last_mention(self, data, match):
+        try:
+            with open(join(settings.DATA_WORKING_DIR, self.filename), 'r') as f:
+                return datetime.datetime.fromtimestamp(float(f.read()))
+        except IOError:
+            return 'Wow... Never.'
+
+def main():
+    run_client(GentooService)
+
+
+if __name__ == '__main__':
+    main()
