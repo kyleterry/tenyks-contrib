4a4e450d46229068341dbc2348e9f82926249922
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-01-14 15:11:42 -0800

    Added user stats script

---
+++ src/tenyksscripts/scripts/user_stats.py
@@ -0,0 +1,21 @@
+import requests, json
+
+
+def run(data, settings):
+    if 'user_stats ' in data['payload']:
+        url = None
+        requested_user = data['payload'].split("user_stats ")[1]
+        if data['target'] == '#merveilles':
+            url = 'http://merveill.es/data/{}'.format(requested_user)
+        else:
+            url = 'http://{chan}.shithouse.tv/data/{user}'.format(
+                chan=data['target'].strip("#"), user=requested_user)
+
+        response = requests.get(url).json()
+        data = json.loads(response)
+
+        if data["total_posts"] == 0:
+            return "No data on that user."
+        return "Links: {} First Link: {} Most recent link: {} Average links per hour: {}".format(
+            data["total_posts"], data["first_post_date"], data["most_recent_post"], data["average_posts_per_hour"])
+
