11a813d0fae959ee6481447e3d1dc125c77bc15a
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-04-16 10:56:42 -0700

    Added stallman copypasta.

---
+++ src/tenyksscripts/scripts/stallman.py
@@ -0,0 +1,6 @@
+output = "I'd just like to interject for a moment. What you’re referring to as Linux, is in fact, GNU/Linux, or as I’ve recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX."
+
+def run(data, settings):
+    payload = data['payload'].lower()
+    if 'linux' in payload and 'gnu' not in payload:
+        return 'pong'
