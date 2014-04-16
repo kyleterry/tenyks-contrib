1d1590965260550f997b9522bd60bef520f75084
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-04-16 11:01:39 -0700

    Removed weird unicode.

--- src/tenyksscripts/scripts/stallman.py
+++ src/tenyksscripts/scripts/stallman.py
@@ -1,4 +1,4 @@
-output = "I'd just like to interject for a moment. What you’re referring to as Linux, is in fact, GNU/Linux, or as I’ve recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX."
+output = "I'd just like to interject for a moment. What you're referring to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX."
 
 def run(data, settings):
     payload = data['payload'].lower()
