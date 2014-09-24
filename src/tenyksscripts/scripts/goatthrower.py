4cf8b07e497ed9344621bf4438bf586484bd61dd
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-09-24 10:09:44 -0700

    Goat thrower.

---
+++ src/tenyksscripts/scripts/goatthrower.py
@@ -0,0 +1,23 @@
+import random
+
+def run(data, settings):
+    if data['payload'] == 'goat tower':
+        distance = random.randrange(0, 100)
+        judgement = ""
+        if distance == 0:
+            judgement = "Fucking awful."
+        elif distance > 25 and distance < 50:
+            judgement = "Try to do better next time."
+        elif distance > 50 and distance < 75:
+            judgement = "Not bad. I've seen better."
+        elif distance > 75 and distance < 100:
+            judgement = "Nice throw, idiot. Why are you throwing goats?"
+        elif distance == 100:
+            judgement = "Calm down, kingpin"
+
+        say = '{nick}: You manage to heave {num_goats} goats for {distance}M. {judgement}'.format(
+                nick = data['nick'],
+                distance = distance,
+                judgement = judgement
+        )
+        return say
