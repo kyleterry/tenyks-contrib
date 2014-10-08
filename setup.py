9988a5cddd4554e8ff76a8d972f1529dcf729aec
Kyle Terry <kyle@kyleterry.com>
2014-10-08 13:39:33 -0700

    Should fix scripts

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
-import sys, os
 
 version = '0.1.27'
 
@@ -9,19 +8,20 @@ setup(name='tenyks-contrib',
       description="Contributed services for the tenyks IRC bot",
       long_description="""\
 """,
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='clients tenyks ircbot services tenyks-service',
       author='Kyle Terry',
       author_email='kyle@kyleterry.com',
       url='https://github.com/kyleterry/tenyks-contrib',
       license='LICENSE',
       packages=find_packages('src', exclude=['ez_setup', 'examples', 'tests']),
-      package_dir = {'':'src'},
+      package_dir={'': 'src'},
+      package_data={'src': ['*.txt']},
       include_package_data=True,
       zip_safe=True,
       install_requires=[
           # -*- Extra requirements: -*-
-          'tenyks', # This is until I port everything to tenyksservice
+          'tenyks',  # This is until I port everything to tenyksservice
           'tenyksservice',
           'feedparser',
           'python-dateutil',
