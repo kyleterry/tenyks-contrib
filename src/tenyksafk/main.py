26d7824ca8434bff06409153ee13109cbaefffff
Marshall Bowers <elliott.codes@gmail.com>
2014-10-23 11:36:44 -0400

    Maintain list of AFKers in SQLite

--- src/tenyksafk/main.py
+++ src/tenyksafk/main.py
@@ -1,3 +1,5 @@
+import sqlite3
+from os.path import join
 from tenyksservice import TenyksService, run_service
 
 class AFK(TenyksService):
@@ -12,22 +14,29 @@ class AFK(TenyksService):
     def __init__(self, *args, **kwargs):
         super(AFK, self).__init__(*args, **kwargs)
         self.away = {}
+        self.create_tables(self.fetch_cursor())
 
     def handle_depart(self, data, match):
         nick = data['nick']
 
-        if not (nick in self.away and self.away[nick]):
-            self.send('{nick} is now AFK.'.format(nick=data['nick']), data)    
+        if not self.user_exists(self.fetch_cursor(), nick):
+            self.create_user(self.fetch_cursor(), nick, True)
 
-        self.away[data['nick']] = True
+        if not self.user_away(self.fetch_cursor(), nick):
+            self.send('{nick} is now AFK.'.format(nick=data['nick']), data) 
+
+        self.user_depart(self.fetch_cursor(), nick)
 
     def handle_return(self, data, match):
         nick = data['nick']
 
-        if not (nick in self.away and not self.away[nick]):
+        if not self.user_exists(self.fetch_cursor(), nick):
+            self.create_user(self.fetch_cursor(), nick, False)
+
+        if self.user_away(self.fetch_cursor(), nick):
             self.send('{nick} is no longer AFK.'.format(nick=nick), data)
 
-        self.away[data['nick']] = False
+        self.user_return(self.fetch_cursor(), nick)
 
     def handle_query(self, data, match):
         nick = match.groupdict()['nick']
@@ -39,13 +48,70 @@ class AFK(TenyksService):
             self.send('{nick}\'s status is unknown.'.format(nick=nick), data)
 
     def handle_list(self, data, match):
-        afk_list = {k: v for k, v in self.away.iteritems() if v}.keys()
+        afkers = self.fetch_afk(self.fetch_cursor())
 
-        if len(afk_list) == 0:
+        if len(afkers) == 0:
             self.send('There are currently no AFKers.', data)
         else:
-            afk_list.sort()
-            self.send('AFKers: {afk}'.format(afk=', '.join(afk_list)), data)
+            self.send('AFKers: {afk}'.format(afk=', '.join('%s' % nick for nick in afkers)), data)
+
+     def create_tables(self, cur):
+        table_sql = '''
+        CREATE TABLE IF NOT EXISTS afkers (
+            id INTEGER PRIMARY KEY,
+            nick TEXT,
+            away BOOLEAN
+        );
+        '''
+        cur.executescript(table_sql)
+
+    def fetch_cursor(self):
+        db_file = '{name}.db'.format(name=self.name)
+        conn = sqlite3.connect(db_file)
+        return conn.cursor()
+
+    def create_user(self, cur, nick, away=False):
+        result = cur.execute('''
+            INSERT INTO afkers (nick, away)
+            VALUES (?, ?)
+        ''', (nick, away))
+        result.connection.commit()
+
+    def user_exists(self, cur, nick):
+        result = cur.execute('''
+            SELECT * FROM afkers
+            WHERE nick = ?
+        ''', (nick,))
+        return result.fetchone() is not None
+
+    def user_depart(self, cur, nick):
+        result = cur.execute('''
+            UPDATE afkers SET away = ?
+            WHERE nick = ?
+        ''', (True, nick))
+        result.connection.commit()
+
+    def user_return(self, cur, nick):
+        result = cur.execute('''
+            UPDATE afkers SET away = ?
+            WHERE nick = ?
+        ''', (False, nick))
+        result.connection.commit()
+
+    def user_away(self, cur, nick):
+        result = cur.execute('''
+            SELECT away from afkers
+            WHERE nick = ?
+        ''', (nick,))
+        return result.fetchone()[0]
+
+    def fetch_afk(self, cur):
+        result = cur.execute('''
+            SELECT nick FROM afkers
+            WHERE away = 1
+            ORDER BY nick ASC
+        ''')
+        return result.fetchall();
 
 
 def main():
