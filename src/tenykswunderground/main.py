7d363386756244e2090f913dcf1888f62de58011
Kyle Terry <kyle@kyleterry.com>
2014-10-28 20:07:10 -0700

    Added forecast, settings and yeah

--- src/tenykswunderground/main.py
+++ src/tenykswunderground/main.py
@@ -1,24 +1,35 @@
 import requests
 
-from tenyks.client import Client, run_client
-from tenyks.client.config import settings
+from tenyksservice import TenyksService, run_service, FilterChain
+from tenyksservice.config import settings
 
 
 SEARCH_URL_TEMPLATE = 'http://autocomplete.wunderground.com/aq?query={location}'
 CURRENT_COND_TEMPLATE = 'http://api.wunderground.com/api/{api_key}/conditions{query}.json'
+# Example: Portland, OR is 56.1 F (13.4 C) and Overcast; windchill is NA; winds are Calm
 ALERTS_TEMPLATE = 'http://api.wunderground.com/api/{api_key}/alerts{query}.json'
+FORECAST_TEMPLATE = 'http://api.wunderground.com/api/{api_key}/forecast{query}.json'
 
 TYPE_CURRENT = 'current'
 TYPE_ALERTS = 'alerts'
+TYPE_FORECAST = 'forecast'
 
 
-class TenyksWeather(Client):
+class TenyksWeather(TenyksService):
 
     irc_message_filters = {
-        'current_weather': r'^(current\s)?weather (for\s)?(?P<loc>(.*))$',
-        'weather_alerts': r'^(current\s)?weather alerts (for\s)?(?P<loc>(.*))$',
+        'current_weather': FilterChain([r'^(current\s)?weather (for\s)?(?P<loc>(.*))$', ],
+                                       direct_only=True),
+        'weather_alerts': FilterChain([r'^(current\s)?weather alerts (for\s)?(?P<loc>(.*))$', ],
+                                      direct_only=True),
+        'forecast': FilterChain([r'^forecast (for\s)?(?P<loc>(.*))$', ],
+                                      direct_only=True),
     }
-    direct_only = True
+
+    def __init__(self, *args, **kwargs):
+        super(TenyksWeather, self).__init__(*args, **kwargs)
+        if not settings.WUNDERGROUND_API_KEY:
+            raise Exception("You need to set WUNDERGROUND_API_KEY in settings.py")
 
     def fetch_location(self, location_query):
         search = requests.get(SEARCH_URL_TEMPLATE.format(location=location_query))
@@ -36,13 +47,16 @@ class TenyksWeather(Client):
             data = requests.get(ALERTS_TEMPLATE.format(
                 api_key=settings.WUNDERGROUND_API_KEY,
                 query=location_string))
+        elif data_type == TYPE_FORECAST:
+            data = requests.get(FORECAST_TEMPLATE.format(
+                api_key=settings.WUNDERGROUND_API_KEY,
+                query=location_string))
         if data.status_code == 200:
             return data.json()
 
     def handle_current_weather(self, data, match):
         location = match.groupdict()['loc']
         location_string = self.fetch_location(location)
-        #import ipdb; ipdb.set_trace()
         if location_string:
             current_json = self.fetch_weather_data(TYPE_CURRENT, location_string)
             if current_json:
@@ -73,9 +87,28 @@ class TenyksWeather(Client):
         else:
             self.send('Unknown location', data)
 
+    def handle_forecast(self, data, match):
+        location = match.groupdict()['loc']
+        location_string = self.fetch_location(location)
+        if location_string:
+            forecast_json = self.fetch_weather_data(
+                TYPE_FORECAST, location_string)
+            if forecast_json:
+                template = '{day} - {message}'
+                i = 0
+                for day in forecast_json['forecast']['txt_forecast']['forecastday']:
+                    if i < 5:
+                        self.send(template.format(
+                            day=day['title'], message=day['fcttext']), data)
+                    else:
+                        break
+                    i = i + 1
+        else:
+            self.send('Unknown location', data)
+
 
 def main():
-    run_client(TenyksWeather)
+    run_service(TenyksWeather)
 
 
 if __name__ == '__main__':
