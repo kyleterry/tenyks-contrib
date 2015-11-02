f9f95e358942f64df738631f4f1fb11b648ff574
Quinlan Pfiffer <qpfiffer@gmail.com>
2015-11-02 11:53:06 -0800

    Update printerfact.py
    Update to fast printer facts API.

--- src/tenyksscripts/scripts/printerfact.py
+++ src/tenyksscripts/scripts/printerfact.py
@@ -2,7 +2,7 @@ import requests, re
 
 def run(data, settings):
     if data['payload'] == 'printer fact':
-        r = requests.get('https://catfacts-api.appspot.com/api/facts?number=1')
+        r = requests.get('http://void.vodka/api/')
         fact = r.json()['facts'][0]
         regex = re.compile(re.escape("cat"), re.IGNORECASE)
         return regex.sub("printer", fact)
