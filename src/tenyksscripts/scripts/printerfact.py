6fd9578bad85010c582f40186acdafded9fa8d23
Colby Olson <me@colbyolson.com>
2016-10-03 18:18:37 -0700

    fuck a type

--- src/tenyksscripts/scripts/printerfact.py
+++ src/tenyksscripts/scripts/printerfact.py
@@ -4,7 +4,7 @@ import requests
 def run(data, settings):
     if data['payload'] == 'printer fact':
         r = requests.get('https://colbyolson.com/printers')
-        if r.status_code == '200':
+        if r.status_code == 200:
             return r.text
         else:
             return 'Bad kitty! Endpoint returned HTTP %s' % (r.status_code)
