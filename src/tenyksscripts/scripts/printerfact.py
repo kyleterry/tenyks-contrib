93275f3482d49b98715e4c8846cc6b2c6cd40275
Colby Olson <me@colbyolson.com>
2016-10-03 18:04:30 -0700

    a silly fix

--- src/tenyksscripts/scripts/printerfact.py
+++ src/tenyksscripts/scripts/printerfact.py
@@ -2,7 +2,5 @@ import requests, re
 
 def run(data, settings):
     if data['payload'] == 'printer fact':
-        r = requests.get('http://void.vodka/api/')
-        fact = r.json()['facts'][0]
-        regex = re.compile(re.escape("cat"), re.IGNORECASE)
-        return regex.sub("printer", fact)
+        r = requests.get('https://colbyolson.com/printers')
+        return r.text
