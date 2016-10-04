bc52c2585c2d48d79d422b81a38c624ac7cf6a88
Colby Olson <me@colbyolson.com>
2016-10-03 18:15:26 -0700

    better python, check the endpoint

--- src/tenyksscripts/scripts/printerfact.py
+++ src/tenyksscripts/scripts/printerfact.py
@@ -1,6 +1,10 @@
-import requests, re
+import requests
+
 
 def run(data, settings):
     if data['payload'] == 'printer fact':
         r = requests.get('https://colbyolson.com/printers')
-        return r.text
+        if r.status_code == '200':
+            return r.text
+        else:
+            return 'Bad kitty! Endpoint returned HTTP %s' % (r.status_code)
