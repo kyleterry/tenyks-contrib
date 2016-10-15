dca34aa57668cffc6d7e957c82bb21c22efb8cc1
Kyle Terry <kyle@kyleterry.com>
2016-10-14 18:57:11 -0700

    add frowns and more smiles

--- src/tenyksscripts/scripts/smile.py
+++ src/tenyksscripts/scripts/smile.py
@@ -1,8 +1,11 @@
 import random
 
-smiles = [':^)', ':-D', ':)', ':P', '(^:', 'B-)']
+smiles = [':^)', ':-D', ':D', ':)', ':P', '(^:', 'B-)']
+frowns = [':^(', 'D-:', 'D:', ':(', ':-/', ')^:', 'B-(', ':\'(']
 
 
 def run(data, settings):
-    if ':^)' == data['payload']:
+    if data['payload'] in smiles:
         return random.choice(smiles)
+    if data['payload'] in frowns:
+        return random.choice(frowns)
