649a2d32567f48f521ce81f86c299e13bd0cf2f4
Kyle Terry <kyle@kyleterry.com>
2017-05-26 23:33:39 -0700

    update shithouse fetcher

--- src/tenyksscripts/scripts/shitlinks.py
+++ src/tenyksscripts/scripts/shitlinks.py
@@ -1,25 +1,35 @@
-import re
 import random
 import requests
 import json
-try:
-    r = requests.get('http://api.shithouse.tv')
 
 
-    #Solution with json parsing
-    parsed_json = json.loads(r.text)
-    num_memes = len(parsed_json)
-    rnd = random.randrange(0, num_memes)
-    if parsed_json[rnd]['nsfw']:
-        shit_links = "{nsfw} {bump}.shithouse.tv".format(bump = parsed_json[rnd]['name'], nsfw = "NSFW")
-    else:
-        shit_links = "{bump}.shithouse.tv".format(bump = parsed_json[rnd]['name'])
-except:
-    shit_links = "shit's fucked bro"
-    num_memes = "fuck you"
+def gimme():
+    try:
+        r = requests.get('http://api.shithouse.tv')
+
+        # Solution with json parsing
+        parsed_json = json.loads(r.text)
+        num_memes = len(parsed_json)
+        rnd = random.randrange(0, num_memes)
+        shit_link = []
+
+        if parsed_json[rnd]['nsfw']:
+            shit_link.append('\x0304[NSFW]\x0F')
+
+        shit_link.append('\x0310http://{0}.shithouse.tv'.format(parsed_json[rnd]['name']))
+    except:
+        shit_link = "shit's fucked bro"
+        num_memes = "fuck you there's none"
+
+    return (' '.join(shit_link), num_memes)
+
+
 def run(data, settings):
+    answer = gimme()
+
     if 'dank memes' in data['payload'].lower():
-        return shit_links
-#todo add more SICK STATZ
+        return answer[0]
+
+    # todo add more SICK STATZ
     if 'shithouse stats' in data['payload'].lower():
-	return "Total Memes: {num}".format(num = num_memes)
+        return "Total Memes: {0}".format(answer[1])
