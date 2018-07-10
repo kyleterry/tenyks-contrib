c1152656f3fe7135b5c3331a9fc215666c6df532
Eric Butera <eric@bytepro.net>
2018-07-10 13:57:57 -0400

    i guess i should check for the date or whatever

--- src/tenyksscripts/scripts/mydude.py
+++ src/tenyksscripts/scripts/mydude.py
@@ -1,9 +1,13 @@
+from datetime import date
+
+WEDNESDAY = 2
+
 def run(data, settings):
     if data['payload'].startswith('is it wednesday'):
-        mydude = 'https://www.youtube.com/watch?v=EBNEPil4da0&list=PLy3-VH7qrUZ5IVq_lISnoccVIYZCMvi-8'
-    else:
-        mydude = 'no'
+        if date.today().weekday() is WEDNESDAY:
+            mydude = 'https://www.youtube.com/watch?v=EBNEPil4da0&list=PLy3-VH7qrUZ5IVq_lISnoccVIYZCMvi-8'
+        else:
+            mydude = 'no'
 
-    return '{nick}: {mydude}'.format(nick=data['nick'], 
-            mydude=mydude)
-
+        return '{nick}: {mydude}'.format(nick=data['nick'], 
+                mydude=mydude)
